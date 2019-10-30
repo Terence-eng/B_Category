0d7ce6fa45e7418e638823df500c1c616e289528
chenweixin <chenweixin@turingcat.com>
2019-10-30 11:01:13 +0800

    init

---
+++ B_Category/Classes/CTMediator+B_Category.h
@@ -0,0 +1,16 @@
+//
+//  CTMediator+B_Category.h
+//  B_Category
+//
+//  Created by Terrence on 2019/10/30.
+//
+
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CTMediator (B_Category)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ B_Category/Classes/CTMediator+B_Category.m
@@ -0,0 +1,12 @@
+//
+//  CTMediator+B_Category.m
+//  B_Category
+//
+//  Created by Terrence on 2019/10/30.
+//
+
+#import "CTMediator+B_Category.h"
+
+@implementation CTMediator (B_Category)
+
+@end
